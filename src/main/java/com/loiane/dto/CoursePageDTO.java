7d0b8f16acf4f06cb697c1071a41b4b22c020102
Wellington Cruz <wellington.max1@gmail.com>
2026-02-22 17:55:58 -0300

    Applying annotation JsonProperty for CoursePageDTO.java

--- src/main/java/com/loiane/dto/CoursePageDTO.java
+++ src/main/java/com/loiane/dto/CoursePageDTO.java
@@ -1,8 +1,11 @@
 package com.loiane.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record CoursePageDTO(
+        @JsonProperty("courses")
         List<CourseDTO> courseDTOS,
         long totalElements,
         int totalPages) {
